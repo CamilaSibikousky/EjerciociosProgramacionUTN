abba574d1f54feb9506d034171ecc85bf0ecb7c7
CamilaSibikousky <sibikousky20@gmail.com>
2025-05-18 03:33:13 -0300

    Ejercicios tema 1 -condicionales

---
+++ Ejercicio_condicionales.py
@@ -0,0 +1,39 @@
+#EJERCICIO 1 if / else / elif
+
+"""
+A partir del ingreso de la altura en centímetros de un jugador de baloncesto, el programa deberá determinar la posición del jugador en la cancha, considerando los siguientes parámetros:
+Menos de 160 cm: Base
+Entre 160 cm y 179 cm: Escolta
+Entre 180 cm y 199 cm: Alero
+200 cm o más: Ala-Pívot o Pívot
+
+"""
+
+altura = float(input("Ingresa la altura en centimeros: "))
+
+if altura < 160:
+    print("Tu posicion es BASE")
+elif 160 <= altura <= 179:
+    print("Tu posicion es ESCOLTA")
+elif 180 <= altura <= 199:
+    print("Tu posicion es ALERO")
+else:
+    print("Tu posicion es PIVOT")
+
+#EJERCICIO 2 if / else / elif
+
+"""
+Calcular una nota aleatoria entre el 1 y el 10 inclusive, para luego mostrar un mensaje según el valor:
+6, 7, 8, 9 y 10  ---> Promoción directa, la nota es ...
+4 y 5            ---> Aprobado, la nota es ...
+1, 2 y 3         ---> Desaprobado, la nota es ...
+
+"""
+nota = float(input("Ingresa la nota del estudiante: "))
+
+if nota < 4:
+    print("Desaprobado, la nota es: ", nota)
+elif 4 <= nota <= 5:
+    print("Aprobado, la nota es: ", nota)
+else:
+    print("Promoción directa, la nota es ", nota)
